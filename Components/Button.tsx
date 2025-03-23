5a2a7d275fef009ec5b44cedac123af5fd204c06
mdaminul1024 <mdfahad1024@gmail.com>
2025-03-23 12:55:16 +0600

    layout and home section setup complete

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
-import { FiUser } from 'react-icons/fi';
 
 type ButtonProps = {
     type: 'button' | 'submit';
     title: string;
     icon?: string;
-    variant?: 'btn-green-600';
 }
 
-const Button = ({type, title, icon, variant}: ButtonProps) => {
+const Button = ({type, title, icon}: ButtonProps) => {
     return (
         <button className="px-6 py-2 bg-black text-white rounded-full flex items-center gap-1 cursor-pointer hover:bg-green-600 font-[400] duration-300 transition-all" type={type}>
             {icon && <Image src={icon} alt={title} width={24} height={24} /> }
