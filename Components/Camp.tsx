7070cec358555868a7afd65ca804c543fb4a1743
mdaminul1024 <mdfahad1024@gmail.com>
2025-03-25 01:29:28 +0600

    working on camp section

--- Components/Camp.tsx
+++ Components/Camp.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+interface CampProps{
+    backgroundImage: string,
+    title: string,
+    subtitle: string,
+    peopleJoined: string,
+}
+
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) =>{
+    return(
+        <div className={`h-full w-full min-w-[400px] md:min-w-[900px] bg-cover bg-no-repeat rounded-2xl ${backgroundImage}`}>
+            {/* Camp site 1 */}
+        </div>
+    )
+}
 
 const Camp = () => {
     return (
-        <div>
-            Camp
-        </div>
+        <section className='my-10 mx-10 md:w-[1100px] md:mx-auto'>
+            <div className='flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px]'>
+                <CampSite 
+                    backgroundImage="bg-bg-img-1"
+                    title="Putuk Truno Camp"
+                    subtitle="Prieen Pasuruan"
+                    peopleJoined="50+ Joined"
+                 />
+                <CampSite 
+                    backgroundImage="bg-bg-img-2"
+                    title="Putuk Truno Camp"
+                    subtitle="Prieen Pasuruan"
+                    peopleJoined="50+ Joined"
+                 />
+            </div>
+        </section>
     );
 };
 
--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -12,11 +12,11 @@ const Hero = () => {
         <div className='md:max-w-[1100px] md:mx-auto mx-10'>
             <div className='md:flex flex-between gap-10 items-center md:mt-0 mt-5'>
 
-                <aside>
+                <aside className='leftAos'>
                     <div className='relative z-20'>
                         <Image src={campImg} alt='camp vector image' width={50} className='absolute left-[-5px] top-[0px] w-10 lg:w-[50px]' />
                         <h1 className='pt-12 text-4xl font-bold text-green-900'>Putuk Truno Camp Area</h1>
-                        <p className='text-sm text-slate-500 mt-2'>We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app.</p>
+                        <p className='text-sm text-slate-600 mt-2'>We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app.</p>
                         <div className='flex gap-1 text-xl mt-2 items-center'>
                             <MdOutlineStarPurple500 className='text-yellow-400'/>
                             <MdOutlineStarPurple500 className='text-yellow-400'/>
@@ -37,9 +37,9 @@ const Hero = () => {
                     </div>
                 </aside>
 
-                <aside className='relative'>
+                <aside className='relative rightAos'>
                     <div className='pt-[-100px]'>
-                        <Image src={RightSideHeroImg} className='w-[1400px]' alt='right side image' />
+                        <Image src={RightSideHeroImg} className='w-[1400px] fadeRight' alt='right side image' />
 
                         <div className='absolute md:top-16 top-4 md:left-10 bg-black backdrop-blur-2xl text-white md:p-5 p-3 rounded-xl shadow-green-300 shadow-2xl'>
                             <MdClose className='text-white absolute right-5 md:top-4 top-3'/>
@@ -60,6 +60,7 @@ const Hero = () => {
                     </div>
                 </aside>
             </div>
+            
         </div>
     );
 };
